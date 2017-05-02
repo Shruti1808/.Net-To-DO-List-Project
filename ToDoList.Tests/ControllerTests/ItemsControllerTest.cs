812b2dfb6119745dda6ca0142499a21e332a8ab9
Grinil Khanna and Shruti Priya <grinil+shruti@>
2017-05-02 09:12:08 -0700

    Add migrations

---
+++ ToDoList.Tests/ControllerTests/ItemsControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using Xunit;
+
+
+namespace ToDoList.Tests
+{
+    public class ItemsControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            //Arrange
+            ItemsController controller = new ItemsController();
+            IActionResult actionResult = controller.Index();
+            ViewResult indexView = controller.Index() as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsType<List<Item>>(result);
+        }
+    }
+}
